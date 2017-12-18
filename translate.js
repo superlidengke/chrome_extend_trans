ed15676b5c0ef512a4ba23b6e08cc0c6c365f53f
Dengke.Li <Dengke.Li@thomsonreuters.com>
2017-12-18 10:58:49 +0800

    lookup from local

--- translate.js
+++ translate.js
@@ -6,21 +6,20 @@ HJ.fun = {
 	// ajax回调函数
 	jsonCallBack : function(json) {
 		var meaning = json.content;
+		console.debug(meaning)
 		showMeaning(meaning);
-		
+		//如果查到单词
 		if(!meaning.includes('hjd_noresult')){
 			add(wordName, json.content);
+			//上传
+			$.post("http://localhost/jp/index.php/Home/Word/insert", {name:wordName, meaning: json.content },
+					   function(data){
+						 
+					   });
 		}
 		
 		next_ready=true;
-		// localStorage.result = json.content;
-		// console.debug(localStorage["result"]);
-		/*
-		$.post("http://localhost/redbag/test/savedata.php", { meaning: json.content },
-				   function(data){
-					 
-				   });
-				   */
+		
 	},
 	changeLanguage : function() {
 
@@ -90,9 +89,35 @@ var topPt;
 			meaningBox.style.top = topPt;
 			$("#meaning").show();
 	}
-	
+	function lookupFromhujiang(param){
+		var query_url = "http://dict.hjenglish.com/services/simpleExplain/jp_simpleExplain.ashx?type=jc&w="
+			+ param;
+	// showMeaning(data);
+	// add(param, data);
+	$.ajax({
+		type : "get",
+		async : true,
+		url : query_url,
+		//dataType : "jsonp",
+		//jsonp : "callback",// 传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
+		success : function(json) {
+			eval(json);				
+		},
+		error : function(data) {
+		}
+	});
+	}
+	function lookupFromMine(param){
+		console.debug("will look up soon")
+		$.post("http://localhost/jp/index.php/Home/Dict/simple", {name:param},
+			function(data){
+				showMeaning(data);
+				console.debug("successfully lookup")
+			});
+		next_ready=true;
+	}
 	function lookup(param){
-		if(param.length>15){
+		if(param.length>15||param.length<1){
 			return;
 		}
 		var nameVal = getValue(param);
@@ -104,26 +129,11 @@ var topPt;
 			}
 			wordName=param;		
 			next_ready=false;
-			var query_url = "http://dict.hjenglish.com/services/simpleExplain/jp_simpleExplain.ashx?type=jc&w="
-					+ param;
-			// showMeaning(data);
-			// add(param, data);
-			$.ajax({
-				type : "get",
-				async : true,
-				url : query_url,
-				//dataType : "jsonp",
-				//jsonp : "callback",// 传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
-				success : function(json) {
-					eval(json);				
-				},
-				error : function(data) {
-				}
-			});
-
+			lookupFromMine(param)
 		}
-	 
 	}
+
+	
 	var selectedTranslate = function(eleContainer) {
 		eleContainer = eleContainer || document;
 		var funGetSelectTxt = function() {
